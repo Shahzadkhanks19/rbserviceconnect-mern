c575a746252c0d5a290b6fa6b8437fa95d767acb
Shahzad Khan <joykhan78600@gmail.com>
2026-08-18 18:14:53 +0530

    Optimize notification unread queries

--- server/src/models/Notification.js
+++ server/src/models/Notification.js
@@ -11,4 +11,5 @@ const notificationSchema = new mongoose.Schema({
 },{timestamps:true});
 
 notificationSchema.index({user:1,createdAt:-1});
+notificationSchema.index({user:1,readAt:1,createdAt:-1});
 export default mongoose.model('Notification',notificationSchema);
