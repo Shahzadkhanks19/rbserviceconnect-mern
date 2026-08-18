e2ccf598ee96f79e7c2ec792afec9a6f95b465e7
Shahzad Khan <joykhan78600@gmail.com>
2026-08-18 18:20:02 +0530

    Optimize production bundle chunks

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,20 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins:[react(),tailwindcss()],
+  build:{
+    sourcemap:false,
+    rolldownOptions:{
+      output:{
+        codeSplitting:{
+          groups:[
+            {name:'react-vendor',test:/node_modules[\\/]react(?:-dom)?[\\/]|node_modules[\\/]react-router(?:-dom)?[\\/]/},
+            {name:'icons-vendor',test:/node_modules[\\/]lucide-react[\\/]/},
+            {name:'realtime-vendor',test:/node_modules[\\/]socket\.io-client[\\/]/},
+          ],
+        },
+      },
+    },
+  },
   server:{
     port:5173,
     proxy:{
