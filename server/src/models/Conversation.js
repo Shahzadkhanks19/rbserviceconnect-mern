6acf4a39e54981db83d6f64ec120a50f4bcbe244
Shahzad Khan <joykhan78600@gmail.com>
2026-08-18 18:01:56 +0530

    Add per-user conversation archive state

--- server/src/models/Conversation.js
+++ server/src/models/Conversation.js
@@ -5,6 +5,7 @@ const conversationSchema=new mongoose.Schema({
   candidate:{type:mongoose.Schema.Types.ObjectId,ref:'User',required:true,index:true},
   job:{type:mongoose.Schema.Types.ObjectId,ref:'Job',default:null,index:true},
   application:{type:mongoose.Schema.Types.ObjectId,ref:'Application',default:null,index:true},
+  archivedBy:[{type:mongoose.Schema.Types.ObjectId,ref:'User'}],
   lastMessageAt:{type:Date,default:Date.now,index:true},
   lastMessagePreview:{type:String,trim:true,maxlength:240,default:''}
 },{timestamps:true});
